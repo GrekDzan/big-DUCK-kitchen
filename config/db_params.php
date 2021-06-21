b5502d12322899083ff31093c00f3a88929b615b
GrekDzan <84064510+GrekDzan@users.noreply.github.com>
2021-06-21 23:17:09 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -2,8 +2,8 @@
 
 // Массив с параметрами подключения к базе данных
 return array(
-    'host' => 'localhost',
-    'dbname' => 'phpshop',
-    'user' => 'root',
-    'password' => 'root',
+    'host' => 'eu-cdbr-west-01.cleardb.com',
+    'dbname' => 'heroku_4eb426ed3ade36a',
+    'user' => 'bbe8ac92a80079',
+    'password' => '2a4e945b',
 );
