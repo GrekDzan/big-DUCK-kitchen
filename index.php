65d9790ec2315c4effed4fa08e85acf0b37d2aac
GrekDzan <84064510+GrekDzan@users.noreply.github.com>
2021-06-23 01:17:41 +0300

    f

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ session_start();
 // Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 // require_once(ROOT.'/components/Autoload.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once (ROOT. '/vendor/autoload.php');
 $array_paths = array('models', 'components', 'controllers');
 
 foreach ($array_paths as $path) {
--- views/site/about.php
+++ views/site/about.php
@@ -14,20 +14,15 @@
                 <br/>
 
                 <div class="map">
-                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab56b4ad989ef58f5f27c8bacf5e8c30f63c60be1ff607307d7d1de5ea4a8e315&amp;width=600&amp;height=348&amp;lang=ru_RU&amp;scroll=true"></script>
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab56b4ad989ef58f5f27c8bacf5e8c30f63c60be1ff607307d7d1de5ea4a8e315&amp;width=500&amp;height=348&amp;lang=ru_RU&amp;scroll=true"></script>
                 </div>
 
                 <br/>
 
-                <p>Phasellus iaculis purus elementum felis varius, vel viverra massa 
-                    tincidunt. Aliquam vulputate dictum luctus. Aenean lectus 
-                    risus, pellentesque eget lectus eu, sodales fringilla magna. 
-                    Nunc diam mi, dictum ac est non, molestie eleifend ligula.
-                    Nam pharetra vulputate lectus. Duis non quam in est interdum 
-                    pellentesque condimentum a elit. Praesent eget turpis euismod, 
-                    maximus lectus sed, elementum mi. Nulla enim tortor, malesuada 
-                    suscipit finibus at, eleifend eget urna. Mauris laoreet metus 
-                    a nisl bibendum, nec maximus urna molestie.</p>
+                <p>Разработка комфортных и практичных кухонь, специально длдя вас , на основе пожеланий заказчика.
+Наша компания готова предложить вам широкий спектр услуг.
+Наши квалифицированные мастера - установщики соберут и установят кухню, а также навесят шкафы-купе, кухонные гарнитуры, шкафы и другую корпусную мебель.
+Вы всегда можете рассчитывать на компетентное и вежливое обслуживание.</p>
             </div>
         </div>
     </div>
